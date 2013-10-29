e414911b3943db85c1f0e5e09d6cdf5b14e7b17f
CSCrew <cscrew@ip0af5023c.int.uvm.edu>
2013-10-29 10:24:37 -0400

    worked on the GUI

---
+++ CatFinder2/CatFinder2AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CatFinder2AppDelegate.h
+//  CatFinder2
+//
+//  Created by CSCrew on 10/29/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CatFinder2AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CatFinder2/CatFinder2AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  CatFinder2AppDelegate.m
+//  CatFinder2
+//
+//  Created by CSCrew on 10/29/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "CatFinder2AppDelegate.h"
+
+@implementation CatFinder2AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
